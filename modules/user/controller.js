91951812a2f0181973284e91456436fb31bdf8d0
hvu160699 <hvu160699@gmail.com>
2019-10-27 11:50:58 +0700

    .

--- modules/user/controller.js
+++ modules/user/controller.js
@@ -64,7 +64,7 @@ router.post('/register', (req, res) => {
 })
 
 router.post('/login', (req, res) => {
-    User.find({
+    User.findOne({
         where: {
             username: req.body.username
         }
